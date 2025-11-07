dbd9181a780cb26299edf5ccf3369b4dec77efc8
Remstedt, Alex <arem@allseas.com>
2025-11-07 13:33:05 +0100

    Update .config/zsh/conf.d/05_bindkeys.zsh

--- home/private_dot_config/zsh/conf.d/05_bindkeys.zsh
+++ home/private_dot_config/zsh/conf.d/05_bindkeys.zsh
@@ -15,6 +15,6 @@ bindkey '^[[B' history-beginning-search-forward-end
 # History substring search
 bindkey '^P' history-substring-search-up
 bindkey '^N' history-substring-search-down
-bindkey '^g' "lazygit\n"
+bindkey -s '^g' "lazygit\n"
 
 bindkey -s '^f' "tmux-sessionizer\n"
